307a23b6bee6cc648ab26ba6e57ba0a38e6c8358
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:02:54 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-session-cache for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-session-cache/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-session-cache/blob/master/LICENSE.md New BSD License
- */
-
 namespace Mezzio\Session\Cache\Exception;
 
 use InvalidArgumentException as PhpInvalidArgumentException;
--- test/CacheSessionPersistenceTest.php
+++ test/CacheSessionPersistenceTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-session-cache for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-session-cache/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-session-cache/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace MezzioTest\Session\Cache;
